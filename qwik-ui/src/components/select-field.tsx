b7a7ff66e00323fd3a8510c911b635c16d71abd4
Erik Jensen <365211+erikrj@users.noreply.github.com>
2026-03-15 12:45:24 -0600

    Refactored SelectField

--- qwik-ui/src/components/select-field.tsx
+++ qwik-ui/src/components/select-field.tsx
@@ -1,66 +1,31 @@
-import {
-  component$,
-  QRL,
-  Signal,
-  Slot,
-  useSignal,
-  useTask$,
-} from '@builder.io/qwik';
+import {component$, Slot} from '@builder.io/qwik';
+import type {PropsOf} from '@builder.io/qwik';
 
-export interface SelectFieldProps {
-  id?: string;
+export interface SelectFieldProps extends Omit<PropsOf<'select'>, 'children'> {
   label?: string;
-  name?: string;
-  value?: string | null | Signal<string | null | undefined>;
   error?: string;
-  required?: boolean;
-  disabled?: boolean;
-  onChange$?: QRL<(event: Event, element: HTMLSelectElement) => void>;
-  onBlur$?: QRL<(event: FocusEvent, element: HTMLSelectElement) => void>;
 }
 
 export const SelectField = component$((props: SelectFieldProps) => {
-  const value = useSignal<string | null | undefined>(
-    typeof props.value === 'string' ? props.value : props.value?.value,
-  );
-  // Synchronize local value to props value
-  useTask$(({track}) => {
-    if (props.value && typeof props.value !== 'string') {
-      track(() => value.value);
-      if (value.value !== props.value?.value) {
-        props.value.value = value.value;
-      }
-    }
-  });
-  // Synchronize props value to local value
-  useTask$(({track}) => {
-    if (props.value && typeof props.value !== 'string') {
-      track(() => (props.value as Signal).value);
-      if (props.value.value !== value.value) {
-        value.value = props.value.value;
-      }
-    }
-  });
+  const {label, error, id, class: className, ...selectProps} = props;
+
   return (
     <div class="fieldset">
-      {props.label && (
-        <label class="label" {...(props.id && {for: props.id})}>
-          <span class="label-text">{props.label}</span>
+      {label && (
+        <label class="label" for={id}>
+          <span class="label-text">{label}</span>
         </label>
       )}
+
       <select
-        {...(props.name && {name: props.name})}
-        {...(props.id && {id: props.id})}
-        required={props.required}
-        aria-required={props.required}
-        disabled={props.disabled}
-        class={`select ${props.error ? 'select-error' : ''}`}
-        onChange$={props.onChange$}
-        onBlur$={props.onBlur$}
+        id={id}
+        class={`select ${error ? 'select-error' : ''} ${className ?? ''}`}
+        {...selectProps}
       >
         <Slot />
       </select>
-      {props.error && <div class="text-error mt-1 text-xs">{props.error}</div>}
+
+      {error && <div class="text-error mt-1 text-xs">{error}</div>}
     </div>
   );
 });
