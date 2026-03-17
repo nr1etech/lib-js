f44b17879c157a015cdd7047e649291dd4319e7a
Erik Jensen <365211+erikrj@users.noreply.github.com>
2026-03-16 21:32:19 -0600

    Fixed issue with Locale type export

---
+++ .changeset/cuddly-shoes-cut.md
@@ -0,0 +1,6 @@
+---
+"@nr1e/commons": patch
+---
+
+Fixed issue with Locale type export
+  
--- commons/src/valibot/locale.ts
+++ commons/src/valibot/locale.ts
@@ -1,4 +1,16 @@
 import {locales, getLocale, setLocale} from '../paraglide/runtime.js';
 
-export type Locale = (typeof locales)[number];
+export type Locale =
+  | 'af'
+  | 'ar'
+  | 'en'
+  | 'en-GB'
+  | 'en-US'
+  | 'en-ZA'
+  | 'es'
+  | 'es-US'
+  | 'fr'
+  | 'fr-CA'
+  | 'fr-FR';
+
 export {locales, getLocale, setLocale};
